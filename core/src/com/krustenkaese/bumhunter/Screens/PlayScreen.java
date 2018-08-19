2c3930789b08742145fa4526a5d756f066c411f6
Krustenkäse <sim>
2018-08-20 00:27:11 +0200

    d

--- core/src/com/krustenkaese/bumhunter/Screens/PlayScreen.java
+++ core/src/com/krustenkaese/bumhunter/Screens/PlayScreen.java
@@ -78,7 +78,7 @@ public class PlayScreen implements Screen {
 
         }
         for(MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            Rectangle rect = ((RectangleMapObject) object).getRectangle(); 
 
             bdef.type = BodyDef.BodyType.StaticBody;
             bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
